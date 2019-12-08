8c03922d3bfe0a76333fdde814e7dbdae9232828
zstromqvist <zakrisstromqvist@gmail.com>
2019-12-08 16:56:09 +0100

    add base day2 puzzle1

---
+++ ruby/day02/day2-1.rb
@@ -0,0 +1,29 @@
+
+array = File.read("test1.txt").split(",").map(&:to_i)
+
+iter = 0
+super_pos = 0
+
+while super_pos != 99 do
+    
+    tmp_array = array[iter..iter+3]
+    super_pos = tmp_array[0]
+    one = tmp_array[1]
+    two = tmp_array[2]
+    pos = tmp_array[3]
+
+    if super_pos == 1
+        array[pos] = array[one] + array[two]
+    elsif super_pos == 2
+        array[pos] = array[one] * array[two]
+    elsif super_pos == 99
+        break
+    else
+        puts "Wrong!"
+        break
+    end
+
+    iter += 4
+end
+
+pp array
